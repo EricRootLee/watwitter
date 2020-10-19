f344659b40c2f3b3ad977d136416b5b3afa3a7e2
German Velasco <germsvel@gmail.com>
2020-10-19 05:39:05 -0400

    Scaffold Timeline and Post with phx.gen
    We scaffold the Timeline and the Post with
    
    ```
    mix phx.gen.live Timeline Post posts username body likes_count:integer
    posts:count_integer
    ```
    
    And we update the migration to be `null: false` and set `default: 0` for
    the counts.

---
+++ priv/repo/migrations/20201019092905_create_posts.exs
@@ -0,0 +1,14 @@
+defmodule Watwitter.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :username, :string, null: false
+      add :body, :string, null: false
+      add :likes_count, :integer, null: false, default: 0
+      add :reposts_count, :integer, null: false, default: 0
+
+      timestamps()
+    end
+  end
+end

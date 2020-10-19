e7a6ba73dc9682a73a161642ef661e34a195d347
German Velasco <germsvel@gmail.com>
2020-10-19 06:04:20 -0400

    Tweak post live test to match style I prefer

--- test/watwitter_web/live/post_live_test.exs
+++ test/watwitter_web/live/post_live_test.exs
@@ -13,27 +13,17 @@ defmodule WatwitterWeb.PostLiveTest do
   }
   @invalid_attrs %{body: nil, likes_count: nil, reposts_count: nil, username: nil}
 
-  defp fixture(:post) do
-    {:ok, post} = Timeline.create_post(@create_attrs)
-    post
-  end
-
-  defp create_post(_) do
-    post = fixture(:post)
-    %{post: post}
-  end
-
   describe "Index" do
-    setup [:create_post]
+    test "lists all posts", %{conn: conn} do
+      post = create(:post)
 
-    test "lists all posts", %{conn: conn, post: post} do
       {:ok, _index_live, html} = live(conn, Routes.post_index_path(conn, :index))
 
       assert html =~ "Listing Posts"
       assert html =~ post.body
     end
 
-    test "saves new post", %{conn: conn} do
+    test "doest not allow blank posts", %{conn: conn} do
       {:ok, index_live, _html} = live(conn, Routes.post_index_path(conn, :index))
 
       assert index_live |> element("a", "New Post") |> render_click() =~
@@ -44,6 +34,15 @@ defmodule WatwitterWeb.PostLiveTest do
       assert index_live
              |> form("#post-form", post: @invalid_attrs)
              |> render_change() =~ "can&apos;t be blank"
+    end
+
+    test "saves new post", %{conn: conn} do
+      {:ok, index_live, _html} = live(conn, Routes.post_index_path(conn, :index))
+
+      assert index_live |> element("a", "New Post") |> render_click() =~
+               "New Post"
+
+      assert_patch(index_live, Routes.post_index_path(conn, :new))
 
       {:ok, _, html} =
         index_live
@@ -55,7 +54,9 @@ defmodule WatwitterWeb.PostLiveTest do
       assert html =~ "some body"
     end
 
-    test "deletes post in listing", %{conn: conn, post: post} do
+    test "deletes post in listing", %{conn: conn} do
+      post = create(:post)
+
       {:ok, index_live, _html} = live(conn, Routes.post_index_path(conn, :index))
 
       assert index_live |> element("#post-#{post.id} a", "Delete") |> render_click()
@@ -64,13 +65,18 @@ defmodule WatwitterWeb.PostLiveTest do
   end
 
   describe "Show" do
-    setup [:create_post]
+    test "displays post", %{conn: conn} do
+      post = create(:post)
 
-    test "displays post", %{conn: conn, post: post} do
       {:ok, _show_live, html} = live(conn, Routes.post_show_path(conn, :show, post))
 
       assert html =~ "Show Post"
       assert html =~ post.body
     end
+  end
+
+  defp create(:post, attrs \\ @create_attrs) do
+    {:ok, post} = Timeline.create_post(attrs)
+    post
   end
 end

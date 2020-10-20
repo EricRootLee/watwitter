e3c368e40826be6abca190152aaa080780adfbf3
German Velasco <germsvel@gmail.com>
2020-10-20 04:52:57 -0400

    Add some bare minimum styles with tailwind cdn

--- lib/watwitter_web/live/post_live/post_component.ex
+++ lib/watwitter_web/live/post_live/post_component.ex
@@ -3,27 +3,27 @@ defmodule WatwitterWeb.PostLive.PostComponent do
 
   def render(assigns) do
     ~L"""
-    <div id="post-<%= @post.id %>" class="post">
-     <div>
-        <div><%= @post.username %></div>
-        <div><%= @post.body %></div>
-      </div>
-      <div>
+    <div id="post-<%= @post.id %>" class="flex flex-col mx-auto">
+      <div class="rounded-lg border-solid border-2 border-gray-100">
         <div>
-          <a href="#" phx-click="like" phx-target="<%= @myself %>">
-            Like <%= @post.likes_count %>
-          </a>
+          <div class="text-gray-700 text-md"><%= @post.username %></div>
+          <div class="flex-1"><%= @post.body %></div>
         </div>
-        <div>
-          <a href="#" phx-click="repost" phx-target="<%= @myself %>">
-            Repost <%= @post.reposts_count %>
-          </a>
-        </div>
-      </div>
+        <div class="flex space-x-4 text-gray-700 text-md">
+          <div>
+            <a href="#" phx-click="like" phx-target="<%= @myself %>">
+              Like <%= @post.likes_count %>
+            </a>
+          </div>
+          <div>
+            <a href="#" phx-click="repost" phx-target="<%= @myself %>">
+              Repost <%= @post.reposts_count %>
+            </a>
+          </div>
 
-      <div>
-        <span><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, @post) %></span>
-        <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></span>
+          <div><%= live_redirect "Show", to: Routes.post_show_path(@socket, :show, @post) %></div>
+          <div><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Are you sure?"] %></div>
+        </div>
       </div>
     </div>
     """

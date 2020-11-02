6397164e7caec569f75aa7b1db32adec9a2f0352
German Velasco <germsvel@gmail.com>
2020-11-02 03:59:21 -0500

    Add repost test and refactor tests
    Adds reposting test and refactor test.
    
    Note that this test is the test for the component. We're testing that
    the user sees the repost increment. For the user's followers, we'll have
    a different test in which we assert that a repost shows up in their
    timeline.

--- test/watwitter_web/live/post_live/post_component_test.exs
+++ test/watwitter_web/live/post_live/post_component_test.exs
@@ -19,8 +19,25 @@ defmodule WatwitterWeb.PostLive.PostComponentTest do
     end
   end
 
-  defp post_card_like(post), do: "#post-#{post.id} [data-role='like-button']"
-  defp post_card_like_count(post), do: "#post-#{post.id} [data-role='like-count']"
+  describe "reposts" do
+    test "user can repost a post", %{conn: conn} do
+      post = create(:post)
+
+      {:ok, view, _} = live(conn, Routes.post_index_path(conn, :index))
+
+      view
+      |> element(post_card_repost(post))
+      |> render_click()
+
+      assert has_element?(view, post_card_repost_count(post), "1")
+    end
+  end
+
+  defp post_card_like(post), do: post_card(post) <> " [data-role='like-button']"
+  defp post_card_like_count(post), do: post_card(post) <> " [data-role='like-count']"
+  defp post_card_repost(post), do: post_card(post) <> " [data-role='repost-button']"
+  defp post_card_repost_count(post), do: post_card(post) <> " [data-role='repost-count']"
+  defp post_card(post), do: "#post-#{post.id}"
 
   defp create(:post) do
     attrs = %{

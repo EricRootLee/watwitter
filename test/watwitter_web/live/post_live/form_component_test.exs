c4f411b20ee64bb3336245b33aa4ec4342cdb722
German Velasco <germsvel@gmail.com>
2020-11-02 04:15:13 -0500

    Add too many chars validation test
    We add a too many characters validation test. We also move all
    `FormCompoment` related tests to a `form_component_test` since that's
    where the events are handled.

---
+++ test/watwitter_web/live/post_live/form_component_test.exs
@@ -0,0 +1,65 @@
+defmodule WatwitterWeb.PostLive.FormComponentTest do
+  use WatwitterWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  @create_attrs %{
+    username: "germsvel",
+    body: "some body"
+  }
+  @invalid_attrs %{username: nil, body: nil}
+  @two_fifty_one ~S"""
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut tortor pretium viverra
+    suspendisse potenti nullam ac. Turpis egestas maecenas pharetra convallis
+    posuere morbi leo urna molestie. Nunc consequat interdumv
+  """
+  @attrs_251_chars %{username: "germsvel", body: @two_fifty_one}
+
+  test "does not allow blank posts", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.post_index_path(conn, :index))
+
+    view
+    |> element("a", "New Post")
+    |> render_click()
+
+    rendered =
+      view
+      |> form("#post-form", post: @invalid_attrs)
+      |> render_change()
+
+    assert rendered =~ "can&apos;t be blank"
+  end
+
+  test "does not allow posts with more than 250 characters", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.post_index_path(conn, :index))
+
+    view
+    |> element("a", "New Post")
+    |> render_click()
+
+    rendered =
+      view
+      |> form("#post-form", post: @attrs_251_chars)
+      |> render_change()
+
+    assert rendered =~ "should be at most 250 character(s)"
+  end
+
+  test "saves new post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.post_index_path(conn, :index))
+
+    view
+    |> element("a", "New Post")
+    |> render_click()
+
+    {:ok, _, html} =
+      view
+      |> form("#post-form", post: @create_attrs)
+      |> render_submit()
+      |> follow_redirect(conn, Routes.post_index_path(conn, :index))
+
+    assert html =~ "Post created successfully"
+    assert html =~ "some body"
+  end
+end
--- test/watwitter_web/live/post_live_test.exs
+++ test/watwitter_web/live/post_live_test.exs
@@ -5,12 +5,6 @@ defmodule WatwitterWeb.PostLiveTest do
 
   alias Watwitter.Timeline
 
-  @create_attrs %{
-    body: "some body",
-    username: "some username"
-  }
-  @invalid_attrs %{body: nil, username: nil}
-
   describe "Index" do
     test "lists all posts", %{conn: conn} do
       post = create(:post)
@@ -19,38 +13,6 @@ defmodule WatwitterWeb.PostLiveTest do
 
       assert html =~ "Listing Posts"
       assert html =~ post.body
-    end
-
-    test "doest not allow blank posts", %{conn: conn} do
-      {:ok, view, _html} = live(conn, Routes.post_index_path(conn, :index))
-
-      view
-      |> element("a", "New Post")
-      |> render_click()
-
-      rendered =
-        view
-        |> form("#post-form", post: @invalid_attrs)
-        |> render_change()
-
-      assert rendered =~ "can&apos;t be blank"
-    end
-
-    test "saves new post", %{conn: conn} do
-      {:ok, view, _html} = live(conn, Routes.post_index_path(conn, :index))
-
-      view
-      |> element("a", "New Post")
-      |> render_click()
-
-      {:ok, _, html} =
-        view
-        |> form("#post-form", post: @create_attrs)
-        |> render_submit()
-        |> follow_redirect(conn, Routes.post_index_path(conn, :index))
-
-      assert html =~ "Post created successfully"
-      assert html =~ "some body"
     end
   end
 
@@ -65,7 +27,12 @@ defmodule WatwitterWeb.PostLiveTest do
     end
   end
 
-  defp create(:post, attrs \\ @create_attrs) do
+  defp create(:post) do
+    attrs = %{
+      username: "germsvel",
+      body: "some body"
+    }
+
     {:ok, post} = Timeline.create_post(attrs)
     post
   end

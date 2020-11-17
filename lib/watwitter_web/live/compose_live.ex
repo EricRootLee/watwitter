4302f7a88d81563c245c5da485f0ad3f9c855263
German Velasco <germsvel@gmail.com>
2020-11-17 12:53:08 -0500

    User can create a new post
    User can create a new post. We also cover validation and back
    navigation, but that could be in a different test.

--- lib/watwitter_web/live/compose_live.ex
+++ lib/watwitter_web/live/compose_live.ex
@@ -1,11 +1,66 @@
 defmodule WatwitterWeb.ComposeLive do
   use WatwitterWeb, :live_view
 
+  alias Watwitter.Accounts
+  alias Watwitter.Timeline
+  alias Watwitter.Timeline.Post
+  alias WatwitterWeb.SVGHelpers
+
+  def mount(_params, session, socket) do
+    current_user = Accounts.get_user_by_session_token(session["user_token"])
+    changeset = Timeline.change_post(%Post{})
+
+    {:ok, assign(socket, changeset: changeset, current_user: current_user)}
+  end
+
   def render(assigns) do
     ~L"""
     <header class="header">
+      <%= live_redirect to: Routes.timeline_path(@socket, :index), id: "back" do %>
+        <%= SVGHelpers.back_svg() %>
+      <% end %>
       <h1 class="header-title">Compose</h1>
     </header>
+
+    <div class="body">
+      <div class="compose-wrapper">
+        <%= f = form_for @changeset, "#", phx_submit: "save", phx_change: "validate", id: "new-post", class: "compose-form" %>
+          <div class="compose-fields">
+            <div class="compose-box">
+              <%= textarea f, :body, placeholder: "What's happening?", class: "compose-textarea" %>
+            </div>
+            <div>
+              <%= error_tag f, :body %>
+            </div>
+          </div>
+
+          <div class="compose-actions">
+            <%= submit "Post", phx_disable_with: "Posting...", class: "compose-btn" %>
+          </div>
+        </form>
+      </div>
+    </div>
     """
+  end
+
+  def handle_event("validate", %{"post" => post_params}, socket) do
+    changeset =
+      %Post{}
+      |> Timeline.change_post(post_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("save", %{"post" => post_params}, socket) do
+    params = Map.put(post_params, "user_id", socket.assigns.current_user.id)
+
+    case Timeline.create_post(params) do
+      {:ok, _post} ->
+        {:noreply, push_redirect(socket, to: Routes.timeline_path(socket, :index))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
---
+++ test/watwitter_web/live/compose_live_test.exs
@@ -0,0 +1,59 @@
+defmodule WatwitterWeb.ComposeLiveTest do
+  use WatwitterWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  setup :register_and_log_in_user
+
+  test "user can navigate back to timeline", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    {:ok, _timeline_view, html} =
+      view
+      |> element("#back")
+      |> render_click()
+      |> follow_redirect(conn, Routes.timeline_path(conn, :index))
+
+    assert html =~ "Home"
+  end
+
+  test "user can compose a new post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    {:ok, timeline_view, _html} =
+      view
+      |> form("#new-post", post: %{body: "This is amazing"})
+      |> render_submit()
+      |> follow_redirect(conn, Routes.timeline_path(conn, :index))
+
+    assert has_element?(timeline_view, ".post", "This is amazing")
+  end
+
+  test "user cannot submit empty post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    rendered =
+      view
+      |> form("#new-post", post: %{body: nil})
+      |> render_submit()
+
+    assert rendered =~ "can&apos;t be blank"
+  end
+
+  @two_hundred_and_fifty_one """
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut tortor pretium
+    viverra suspendisse potenti nullam ac. Turpis egestas maecenas pharetra
+    convallis posuere morbi leonur
+  """
+  test "user sees errors when interacting with form", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    rendered =
+      view
+      |> form("#new-post", post: %{body: @two_hundred_and_fifty_one})
+      |> render_change()
+
+    assert rendered =~ "should be at most 250 character(s)"
+  end
+end

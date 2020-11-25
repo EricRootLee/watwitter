d588548e1b4b3defb54fa020fd192b6a7a1cde6f
German Velasco <germsvel@gmail.com>
2020-11-25 15:46:16 -0500

    Test post creation
    We test that a user can create a new post successfully and that errors
    will be returned on failure.
    
    We see how to use the `form/3` helper, and we notice how it's essential
    to use it that way and not to use `render_submit/3` directly by
    targeting an event.

---
+++ test/watwitter_web/live/compose_live_test.exs
@@ -0,0 +1,30 @@
+defmodule WatwitterWeb.ComposeLiveTest do
+  use WatwitterWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  setup :register_and_log_in_user
+
+  test "user can create post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    {:ok, _view, html} =
+      view
+      |> form("#new-post", post: %{body: "This is awesome"})
+      |> render_submit()
+      |> follow_redirect(conn, Routes.timeline_path(conn, :index))
+
+    assert html =~ "Home"
+  end
+
+  test "user cannot submit empty post", %{conn: conn} do
+    {:ok, view, _html} = live(conn, Routes.compose_path(conn, :new))
+
+    rendered =
+      view
+      |> form("#new-post", post: %{body: nil})
+      |> render_submit()
+
+    assert rendered =~ "can&apos;t be blank"
+  end
+end
